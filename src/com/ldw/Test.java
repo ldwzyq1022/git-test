e5be61738a9dfc75bf97c06486d646e0b57272f8
ldwzyq1022 <2267190751@qq.com>
2024-04-10 08:00:17 +0800

    第一次测试文件

---
+++ src/com/ldw/Test.java
@@ -0,0 +1,13 @@
+package com.ldw;
+
+/**
+ * 功能描述
+ *
+ * @author lidawei
+ * @date 2024/04/10  7:57
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("111111");
+    }
+}
